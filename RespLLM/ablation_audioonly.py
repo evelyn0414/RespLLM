c8a6cc14f5df433339e908bdc8d80dd906ad271b
Yuwei (Evelyn) Zhang <61721952+evelyn0414@users.noreply.github.com>
2025-01-30 13:38:10 +0000

    Update ablation_audioonly.py

--- RespLLM/ablation_audioonly.py
+++ RespLLM/ablation_audioonly.py
@@ -19,7 +19,7 @@ from lightning.pytorch.utilities import CombinedLoader
 import time
 from torch import nn
 from torch.nn import functional as F
-from src.benchmark.llm_eval.util import itr_merge, EarlyStopper, downsample_balanced_dataset, upsample_balanced_dataset, set_all_seed
+from src.benchmark.respllm.util import itr_merge, EarlyStopper, downsample_balanced_dataset, upsample_balanced_dataset, set_all_seed
 from src.benchmark.model_util import get_encoder_path, initialize_pretrained_model
 import random
 from torchmetrics import AUROC
